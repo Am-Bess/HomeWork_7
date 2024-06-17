0d2978e9ad394979e61f00eb471d0aca3b5317ca
AleksandR <Smoker007@ya.ru>
2024-06-17 19:22:17 +0300

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int[] nums = new int[InputSizeArray("Введите длинну массива : ")];
+
+    FillArray(nums);
+
+    Console.Write("Было  => ");
+    PrintArrayNormal(nums);
+
+    Console.Write("\nCтало => ");
+    PrintArrayReverse(nums);
+}
+
+int InputSizeArray(string msg)
+{
+    Console.Write(msg);
+    string? inputUser = Console.ReadLine();
+    return int.Parse(inputUser!);
+}
+
+void FillArray(int[] arr, int index = 0)
+{
+    Random rnd = new Random();
+    if (index == arr.Length) return;
+
+    arr[index] = rnd.Next(1, 100);
+    FillArray(arr, index + 1);
+}
+
+void PrintArrayNormal(int[] arr, int index = 0)
+{
+    if (index == arr.Length) return;
+
+    Console.Write($"{arr[index]}\t");
+    PrintArrayNormal(arr, index + 1);
+}
+
+void PrintArrayReverse(int[] arr, int index = 0)
+{
+    if (index == arr.Length) return;
+
+    PrintArrayReverse(arr, index + 1);
+    Console.Write($"{arr[index]}\t");
+}
+
+Main();
